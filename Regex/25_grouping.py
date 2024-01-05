1464e926033d2e8999ff84f13fbc4b5c0fd16fae
Ravi Kansal <rkansal.pnp@gmail.com>
2024-01-05 13:19:23 +0530

    code fix

--- Regex/25_grouping.py
+++ Regex/25_grouping.py
@@ -3,7 +3,7 @@ import re
 
 data = "Email address: admin1@gmail.com, test@example.com, user1@yahoo.com"
 # pattern = r'\w+@\w+\.\w+'
-pattern = r'(\w+)(@\w+\.)\.(\w+)'
+pattern = r'(\w+)(@\w+\.)(\w+)'
 result = re.findall(pattern, data)
 
 # problem fetch username and domain via group not @
